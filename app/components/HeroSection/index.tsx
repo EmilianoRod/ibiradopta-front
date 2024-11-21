f42c436138331d1096599f959ca00989ed8a6db1
Josefina-Mayans <jmayans98@gmail.com>
2024-11-21 10:46:00 -0300

    landing details

--- app/components/HeroSection/index.tsx
+++ app/components/HeroSection/index.tsx
@@ -1,12 +1,12 @@
 const HeroSection = () => {
     return (  
-      <main className="py-10 bg-cover bg-center bg-no-repeat font-Poppins" style={{ backgroundImage: "url('/arbol.webp')" }}>
-        <div className="pl-24 pr-96 flex flex-col space-y-8 pt-28 drop-shadow-2xl">
+      <main className="py-10 bg-cover bg-center bg-no-repeat font-Poppins" style={{ backgroundImage: "url('/arbol_fondo.png')" }}>
+        <div className="pl-24 pr-96 flex flex-col space-y-8 py-52 drop-shadow-2xl">
           <h1 className="text-white text-3xl font-bold">Plantá un árbol, transformá el futuro</h1>
           <p className="text-white text-2xl">
             Un pequeño gesto puede crear un cambio gigante. Adopta un árbol hoy y forma parte de un movimiento global para restaurar nuestro planeta...
           </p>
-          <button className="bg-moss-green text-white text-3xl w-80 h-16 rounded-full hover:bg-green-700">QUIERO PLANTAR</button>
+          <button className="bg-moss-green text-white text-3xl w-80 h-16 border-dotted border-2 rounded-full hover:bg-green-700">QUIERO PLANTAR</button>
         </div>
       </main>
     );
