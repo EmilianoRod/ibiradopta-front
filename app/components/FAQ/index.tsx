4218ccc18af56e0ac41232f04203e5e8e862df11
jorgevespahurst <jvespahurst@gmail.com>
2024-11-28 23:33:56 -0300

    Responsive

--- app/components/FAQ/index.tsx
+++ app/components/FAQ/index.tsx
@@ -2,9 +2,9 @@ import FAQList from "./FAQList";
 
 function FAQSection() {
     return (
-      <section id="FAQ" className="h-screen bg-gray-100 py-10">
+      <section id="FAQ" className="bg-gray-100 py-10 px-4 lg:px-20 text-center">
         
-        <h1 className="text-moss-green text-4xl text-center font-Poppins font-bold pb-8">Preguntas Frecuentes</h1>
+        <h1 className="text-moss-green text-3xl sm:text-4xl font-bold pb-8">Preguntas Frecuentes</h1>
         <FAQList />
       </section>
     );
--- app/components/PlantingLocations/index.tsx
+++ app/components/PlantingLocations/index.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 function PlantingLocations() {
     return (
-      <section className='h-screen p-10 font-Poppins'>
-        <h1 className="text-moss-green text-center text-3xl font-bold pb-10">¿Dónde plantamos nuestros árboles?</h1>
-        <div className='flex'>
+      <section className='min-h-screen p-6 lg:p-10 font-Poppins"'>
+        <h1 className="text-moss-green text-center text-3xl lg:text-4xl font-bold pb-10">¿Dónde plantamos nuestros árboles?</h1>
+        <div className='grid gap-6 lg:grid-cols-3'>
           <article className='max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden'>
             <Image src="/personas-1.jpeg" alt="personas plantando" width={450} height={200} className='object-cover' />
             <div className='p-8 bg-moss-green'>
@@ -58,14 +58,14 @@ function PlantingLocations() {
     );
   }
   
-  function PlantingLocationCard({ img }: { img: string }) {
+  function PlantingLocationCard({ img, date, location }: { img: string, date: string, location: string }) {
     return (
-      <article className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden">
-        <Image src={`/${img}`} alt="personas plantando" width={450} height={200} className="object-cover" />
+      <article className="bg-white rounded-xl shadow-md overflow-hidden">
+        <Image src={`/${img}`} alt="personas plantando" width={450} height={200} className="object-cover w-full h-48" />
         <div className="p-8 bg-moss-green">
           <div className="flex justify-between uppercase tracking-wide text-sm text-white font-semibold">
-            <div>30 de octubre de 2024</div>
-            <div>Montevideo, Uruguay</div>
+            <div>{date}</div>
+            <div>{location}</div>
           </div>
           <p className="mt-2 text-white pt-5">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eius veritatis animi...
