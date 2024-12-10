10271ec933c73bcc9b252ab43cc06870b8f29961
Leonardo Franco <leonardojfs83@gmail.com>
2024-12-10 16:30:27 -0300

    merge projectManagerPage to dev

--- app/MyDonations/MyDonationsButtom.tsx
+++ app/MyDonations/MyDonationsButtom.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const MyDonationsButton = () => {
     return (
-        <li>
+        <div>
             <Link href="/MyDonations" className="text-moss-green block px-4 py-2 text-lg lg:text-2xl hover:text-green-700">Mis Donaciones</Link>
-        </li>
+        </div>
     )
 }
 
